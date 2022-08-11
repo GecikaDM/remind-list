c7c870685c31b3923501504c1239202538080a18
GecikaDM <kaci_gdm@hotmail.com>
2022-08-11 16:39:13 +0200

    primary-style-and-function

---
+++ script.js
@@ -0,0 +1,49 @@
+const form = document.getElementById('form');
+
+const todo = document.getElementById('todo');
+
+const listItems = document.querySelector('.list-items');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+ 
+  const todoValue = todo.value;
+  
+  // create the element task
+  let item = `
+              <div class="item"> 
+                    <p> ${todoValue} </p>
+                    <button class="btn-delete">
+                      <i class="fas fa-trash-alt"></i>
+                    </button>
+                    <button class="btn-archive">
+                      <i class="fas fa-check-circle"></i>
+                    </button>
+                </div>
+              `
+    listItems.innerHTML += item;
+    todo.value = '';
+
+
+    const btnDelete = document.querySelectorAll('.btn-delete');
+
+     btnDelete.forEach(i => {
+  
+
+          i.addEventListener('click', function() {
+          
+          i.parentElement.remove();
+        });
+      });
+
+    const btnArchive = document.querySelectorAll('.btn-archive');
+
+      btnArchive.forEach(i => {
+        console.log(btnArchive);
+        
+        i.addEventListener('click', function() {
+          i.parentElement.classList.toggle('done');
+        });
+      });
+});
+
